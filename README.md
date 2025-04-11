9aadd9a9924db877604fb9d8eb3516ffd0150548
Spencer Wilson <spencer.wilson@uwaterloo.ca>
2025-04-11 06:13:22 -0400

    Create README and initial script to populate repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# HQC
+
+This repository pulls the HQC reference code from https://pqc-hqc.org and prepares it for inclusion in liboqs.
---
+++ update_code.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wget https://pqc-hqc.org/doc/hqc-submission_2025-02-19.zip
+unzip hqc-submission_2025-02-19.zip -x Hardware_Implementation/* README Supporting_Documentation/*
+rm hqc-submission_2025-02-19.zip
+
+# rename all .cpp files to .c
+for cppfile in Reference_Implementation/*/src/*.cpp
+do
+	cfile=${cppfile%.cpp}.c
+	mv "$cppfile" "$cfile"
+done
+
+# avoid NTL dependency by using GF(2^X) arithmetic from the "additional" implementation
+for addfile in Additional_Implementation/*/src/gf2x.c
+do
+	reffile=Reference_Implementation${addfile#Additional_Implementation}
+	cp "$addfile" "$reffile"
+done
+
+rm -r Additional_Implementation
